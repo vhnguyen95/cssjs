c275ccf8b4e1118c77e5de9edbb5b4c2a38ee6ae
vhnguyen95 <45998247+vhnguyen95@users.noreply.github.com>
2018-12-19 21:32:17 +0700

    Update popupan.js

--- popupan.js
+++ popupan.js
@@ -1,5 +1,5 @@
 
-	var url_popup = 'https://thuephongkhachsandaihan.blogspot.com';
+	var url_popup = 'https://www.facebook.com/vhnguyen.95';
 
       //<![CDATA[
 	  // var url_ppp = 'http://bit.ly/1KmPNYe';
